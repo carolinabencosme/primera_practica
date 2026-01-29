30ef4966735197a98eafba8b05ee6d0958cec9c3
Carolina <cbencome@gmail.com>
2026-01-29 19:36:38 -0400

    Initialize Spring Boot project with Gradle setup and basic application structure

---
+++ src/main/java/org/example/primera_practica/PrimeraPracticaApplication.java
@@ -0,0 +1,13 @@
+package org.example.primera_practica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeraPracticaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimeraPracticaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/primera_practica/PrimeraPracticaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.primera_practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeraPracticaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
